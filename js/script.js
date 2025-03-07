af008b780f56ced839d7b8ca838b7555f918c6a5
rina-parsegian <rina.parsegian@gmail.com>
2025-03-07 13:07:49 -0800

    Added JS to existingcode

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const guessedLettersLog = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordShow = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const remainingGuessesSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgainBtn = document.querySelector(".play-again");
+
+const word = "magnolia";
+
+const placeholder = function (word) {
+  const placeholderLetters = [];
+  for (const letter of word) {
+    console.log(letter);
+    placeholderLetters.push("●");
+  }
+  wordInProgress.innerText = placeholderLetters.join("");
+};
+
+placeholder(word);
+
+guessLetterButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const guess = letterInput.value;
+  console.log(guess);
+  letterInput.value = "";
+});
+
